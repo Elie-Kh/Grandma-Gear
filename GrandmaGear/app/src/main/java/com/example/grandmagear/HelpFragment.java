18948d1439170716f33b24ada3b21e539c953f4d
Elie Kheirallah <kheirallahelie@gmail.com>
2020-02-25 20:55:28 -0500

    Added Help Fragment. Created SharedPreferencesHelper for disclaimer. Tested buttons work.

---
+++ GrandmaGear/app/src/main/java/com/example/grandmagear/HelpFragment.java
@@ -0,0 +1,23 @@
+package com.example.grandmagear;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class HelpFragment extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.help_fragment, container, false);
+        return view;
+    }
+
+}
--- GrandmaGear/app/src/main/java/com/example/grandmagear/MainActivity.java
+++ GrandmaGear/app/src/main/java/com/example/grandmagear/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.grandmagear;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.grandmagear.ui.login.ClientLogin;
 
@@ -13,23 +15,29 @@ public class MainActivity extends AppCompatActivity {
 
     protected Button mPatientButton;
     protected Button mClientButton;
+    protected ImageView mLogo;
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initializeButtons();
+
+        initializePage();
 
     }
 
-    protected void initializeButtons(){
+    protected void initializePage(){
         mPatientButton = findViewById(R.id.patient_button);
         mClientButton = findViewById(R.id.client_button);
+        mLogo = findViewById(R.id.app_logo);
+        mLogo.setImageResource(R.drawable.sooken);
 
         mPatientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
+                Intent intent = new Intent(MainActivity.this, PatientActivity.class);
+                startActivity(intent);
             }
         });
 
