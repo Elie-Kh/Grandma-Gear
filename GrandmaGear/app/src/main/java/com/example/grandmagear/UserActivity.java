54fe591728665e6f39c475d92808b34bf889f463
Elie Kheirallah <kheirallahelie@gmail.com>
2020-02-27 00:10:03 -0500

    Added recyclerview. Added item. Created page for GPS. Need to populate recycler view. Still work in progress. currently app will crash, do not branch off my work yet.

--- GrandmaGear/app/src/main/java/com/example/grandmagear/UserActivity.java
+++ GrandmaGear/app/src/main/java/com/example/grandmagear/UserActivity.java
@@ -1,18 +1,28 @@
 package com.example.grandmagear;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class UserActivity extends AppCompatActivity {
-
-    protected Button mLogoutButton;
+    protected MenuItem mLogout;
     protected FirebaseAuth firebaseAuth;
+
+    protected RecyclerView mRecyclerView;
+    protected RecyclerViewAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +33,9 @@ public class UserActivity extends AppCompatActivity {
     }
 
     void setupUI(){
-        mLogoutButton = findViewById(R.id.logoutButton);
+
         firebaseAuth = FirebaseAuth.getInstance();
 
-        mLogoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logout();
-            }
-        });
     }
 
     public void logout(){
@@ -55,4 +59,21 @@ public class UserActivity extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(), LogInActivity.class));
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.user_action_bar,menu);
+        mLogout = findViewById(R.id.logoutAction);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logoutAction) {// User chose the "Settings" item, show the app settings UI...
+            logout();
+        }// If we got here, the user's action was not recognized.
+        // Invoke the superclass to handle it.
+        return super.onOptionsItemSelected(item);
+    }
+
 }
