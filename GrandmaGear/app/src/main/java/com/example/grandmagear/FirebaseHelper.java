cb3089ce88c74d98ec6c014225af6f896059da54
Elie Kheirallah <kheirallahelie@gmail.com>
2020-03-13 17:52:20 -0400

    Added helper functions in FirebaseHelper.

--- GrandmaGear/app/src/main/java/com/example/grandmagear/FirebaseHelper.java
+++ GrandmaGear/app/src/main/java/com/example/grandmagear/FirebaseHelper.java
@@ -1,9 +1,146 @@
 package com.example.grandmagear;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Transaction;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
 public class FirebaseHelper {
     private static final String userDB = "userDB";
     private static final String deviceDB = "deviceDB";
     private static final String eventDB = "eventDB";
+    protected static FirebaseAuth firebaseAuth;
+    protected static FirebaseFirestore firebaseFirestore;
+
+    public FirebaseHelper() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+    }
+
+    public void AddUser(final FirebaseObjects.UserDBO newUser) {
+        newUser.username = firebaseAuth.getCurrentUser().getUid();
+        DocumentReference documentReference = firebaseFirestore.collection(userDB).document(newUser.username);
+        Map<String,Object> user = new HashMap<>();
+        user.put("Username", newUser.username);
+        user.put("Email", newUser.email);
+        user.put("Name", newUser.name);
+        user.put("Password", newUser.password);
+        user.put("Account Type", newUser.acc_type);
+        user.put("Age", newUser.age);
+        user.put("Height", newUser.height);
+        user.put("Weight", newUser.weight);
+        user.put("Devices Followed", newUser.device_ids);
+        user.put("Events", newUser.events);
 
 
+        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "onSuccess: user Profile is created for " + newUser.username);
+            }
+        });
+        documentReference.set(user).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: " + e.toString());
+            }
+        });
+    }
+
+    public void editUser(final FirebaseObjects.UserDBO userDBO, final String field, final Object newVar){
+        final DocumentReference documentReference = firebaseFirestore.collection(userDB).document(userDBO.username);
+
+        firebaseFirestore.runTransaction(new Transaction.Function<Void>() {
+            @Nullable
+            @Override
+            public Void apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
+                transaction.update(documentReference, field, newVar);
+
+                return null;
+            }
+        }).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "Edit success!");
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Edit failure.", e);
+            }
+        });
+    }
+
+    public void addDeviceFollowed(final FirebaseObjects.UserDBO userDBO, String deviceID){
+        ArrayList<String> devices = userDBO.getDevice_ids();
+        devices.add(deviceID);
+    }
+
+    public void addNotification(final FirebaseObjects.UserDBO userDBO,
+                                FirebaseObjects.Notifications notifications){
+        final DocumentReference documentReference = firebaseFirestore.collection(userDB).document(userDBO.username);
+        ArrayList<FirebaseObjects.EventsDBO> events = userDBO.getEvents();
+        events.add(new FirebaseObjects.EventsDBO(events.size()+1, notifications));
+    }
+
+    public void addDevice(final FirebaseObjects.DevicesDBO device){
+
+        device.deviceID = firebaseAuth.getUid();
+        final DocumentReference documentReference = firebaseFirestore.collection(deviceDB).document(device.deviceID);
+        Map<String,Object> devices = new HashMap<>();
+        devices.put("deviceID", device.deviceID);
+        documentReference.set(devices).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "onSuccess: Device profile is created for " + device.deviceID);
+            }
+        });
+        documentReference.set(devices).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: " + e.toString());
+            }
+        });
+    }
+
+    public void editDevice(FirebaseObjects.DevicesDBO device, final String type, final Object field){
+        final DocumentReference documentReference = firebaseFirestore.collection(userDB).document(device.deviceID);
+
+        firebaseFirestore.runTransaction(new Transaction.Function<Void>() {
+            @Nullable
+            @Override
+            public Void apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
+                transaction.update(documentReference, type, field);
+                return null;
+            }
+        }).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "Edit success!");
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Edit failure.", e);
+                    }
+                });
+    }
 }
--- GrandmaGear/app/src/main/java/com/example/grandmagear/FirebaseObjects.java
+++ GrandmaGear/app/src/main/java/com/example/grandmagear/FirebaseObjects.java
@@ -5,37 +5,97 @@ import android.bluetooth.BluetoothClass;
 import java.util.ArrayList;
 
 public class FirebaseObjects {
-    public class UserDBO{
+    public static class UserDBO{
         protected String username;
         protected String email;
         protected String name;
         protected String password;
         protected Boolean acc_type;
-        protected int age;
-        protected short weight;
-        protected short height;
-        protected ArrayList<DevicesDBO> device_ids;
+        protected int age = 0;
+        protected int weight = 0;
+        protected int height = 0;
+        protected ArrayList<String> device_ids;
         protected ArrayList<EventsDBO> events;
 
+        /*the following constructor is strictly for testing purposes*/
+
+        public UserDBO(String email, String name) {
+            this.email = email;
+            this.name = name;
+        }
+
+        public UserDBO(String username, String email, String name, String password, Boolean acc_type) {
+            this.username = username;
+            this.email = email;
+            this.name = name;
+            this.password = password;
+            this.acc_type = acc_type;
+        }
+
+        public UserDBO(String username, String email, String name, String password, Boolean acc_type, int age, short weight, short height) {
+            this.username = username;
+            this.email = email;
+            this.name = name;
+            this.password = password;
+            this.acc_type = acc_type;
+            this.age = age;
+            this.weight = weight;
+            this.height = height;
+        }
+
+        public void setDevice_ids(ArrayList<String> device_ids) {
+            this.device_ids = device_ids;
+        }
+
+        public void setEvents(ArrayList<EventsDBO> events) {
+            this.events = events;
+        }
+
+        public ArrayList<String> getDevice_ids() {
+            return device_ids;
+        }
+
+        public ArrayList<EventsDBO> getEvents() {
+            return events;
+        }
     }
 
-    public class DevicesDBO{
+    public static class DevicesDBO{
+        protected String deviceID;
         protected int bpm;
-        protected double latGPS;    //latitude
-        protected double lonGPS;    //longitude
+        protected long latGPS;    //latitude
+        protected long lonGPS;    //longitude
         protected int deviceBattery;
         protected int phoneBattery;
         protected ArrayList<Notifications> notifications; //notifications linked to that device only
 
+        public DevicesDBO(String deviceID) {
+            this.deviceID = deviceID;
+        }
+
+
     }
 
-    public class EventsDBO{
+    public static class EventsDBO{
         protected int eventNum;
         protected ArrayList<Notifications> notification; //all notifications of followed devices
         //this list will be displayed in the follower notifications list, descending order.
+
+
+        public EventsDBO(int eventNum, Notifications notifications) {
+            this.eventNum = eventNum;
+            this.notification.add(notifications);
+        }
+
+        public int getEventNum() {
+            return eventNum;
+        }
+        public void setNotification(ArrayList<Notifications> notification) {
+            this.notification = notification;
+        }
     }
 
-    public class Notifications{
+    public static class Notifications{
         protected String notificationType;
         protected String notificationInfo;
 
