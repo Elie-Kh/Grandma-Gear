7743bf89e17317d4d996f12d8ea5e93750f943af
John <johnab4444@gmail.com>
2020-03-17 14:20:44 -0400

    Notifications are placed in reverse order

--- GrandmaGear/app/src/main/java/com/example/grandmagear/NotificationsRecyclerView.java
+++ GrandmaGear/app/src/main/java/com/example/grandmagear/NotificationsRecyclerView.java
@@ -13,10 +13,12 @@ import java.util.ArrayList;
 
 public class NotificationsRecyclerView extends RecyclerView.Adapter<NotificationsRecyclerView.ViewHolder> {
 
-    private ArrayList<String> notifications;
+    private ArrayList<String> notificationTitle;
+    private ArrayList<String> notificationText;
 
-    public NotificationsRecyclerView(ArrayList<String> notifications){
-        this.notifications = notifications;
+    public NotificationsRecyclerView(ArrayList<String> notificationTitle, ArrayList<String> notificationText){
+        this.notificationTitle = notificationTitle;
+        this.notificationText = notificationText;
     }
 
     @NonNull
@@ -30,24 +32,27 @@ public class NotificationsRecyclerView extends RecyclerView.Adapter<Notification
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.mNotification.setText(notifications.get(getItemCount() - position -1));
+        holder.mNotificationTitle.setText(notificationTitle.get(position));
+        holder.mNotificationText.setText(notificationText.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return notifications.size();
+        return (notificationTitle.size() + notificationText.size())/2;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         protected ImageView mPatientImage;
-        protected TextView mNotification;
+        protected TextView mNotificationTitle;
+        protected TextView mNotificationText;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             mPatientImage = itemView.findViewById(R.id.patient_image_notifications);
-            mNotification = itemView.findViewById(R.id.notification);
+            mNotificationTitle = itemView.findViewById(R.id.notificationTitle);
+            mNotificationText = itemView.findViewById(R.id.notificationText);
         }
     }
 }
