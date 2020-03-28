c9eba52de5108bf856c4c59334ed23d7ca8f87bb
Kizito <kizitokabanza@gmail.com>
2020-03-27 23:08:43 -0400

    creating new branch

--- GrandmaGear/app/src/main/java/com/example/grandmagear/Patient_Main_Lobby/HomePage_MPP_1.java
+++ GrandmaGear/app/src/main/java/com/example/grandmagear/Patient_Main_Lobby/HomePage_MPP_1.java
@@ -2,23 +2,37 @@ package com.example.grandmagear.Patient_Main_Lobby;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
+import com.example.grandmagear.FirebaseHelper;
+import com.example.grandmagear.FirebaseObjects;
 import com.example.grandmagear.LogInActivity;
 import com.example.grandmagear.R;
+import com.example.grandmagear.SharedPreferencesHelper;
+import com.example.grandmagear.UserActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomePage_MPP_1 extends AppCompatActivity {
+import static com.example.grandmagear.Patient_Main_Lobby.PatientSettingsActivity.location;
 
+public class HomePage_MPP_1 extends AppCompatActivity {
+    private static final String TAG = "HomePage_MPP_1";
 
     MenuItem Settings;
+    Switch location;
     TextView FullName;
     TextView Age;
     TextView Weight;
@@ -29,7 +43,9 @@ public class HomePage_MPP_1 extends AppCompatActivity {
     ImageView Battery;
     ImageView Earth;
 
-    private boolean location;
+    FirebaseHelper firebaseHelper = new FirebaseHelper();
+    SharedPreferencesHelper mSharedPreferencesHelper_Login;
+    protected FirebaseObjects.UserDBO thisUser;
 
 
 
@@ -38,13 +54,26 @@ public class HomePage_MPP_1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page__mpp_1);
 
-        setUpUI();
 
+        setUpUI();
+        mSharedPreferencesHelper_Login = new SharedPreferencesHelper(HomePage_MPP_1.this, "Login");
+        firebaseHelper.getUser(new FirebaseHelper.Callback_getUser() {
+                                   @Override
+                                   public void onCallback(FirebaseObjects.UserDBO user) {
+                                       thisUser = user;
+                                   }
+                               }, mSharedPreferencesHelper_Login.getEmail(),
+                Boolean.parseBoolean(mSharedPreferencesHelper_Login.getType()));
 
 
 
     }
+    @Override
+    public void onResume() {
+        super.onResume();
 
+            locationStatus(location);
+    }
 
 
     void setUpUI(){
@@ -58,6 +87,7 @@ public class HomePage_MPP_1 extends AppCompatActivity {
         Heart = findViewById(R.id.imageView_Heart_MPP_1);
         Earth = findViewById(R.id.imageView_Earth_MPP_1);
         Battery = findViewById(R.id.imageView_BatteryLevel_MPP_1);
+        location = findViewById(R.id.locationSwitch);
 
         //TODO fetch the boolean location data from the database
         //location =
@@ -70,6 +100,9 @@ public class HomePage_MPP_1 extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.patient_action_bar,menu);
         Settings = findViewById(R.id.settings);
         return super.onCreateOptionsMenu(menu);
+
+
+
     }
 
     @Override
@@ -92,11 +125,58 @@ public class HomePage_MPP_1 extends AppCompatActivity {
     }
 
 
-    public boolean isLocation() {
-        return location;
+
+    public void locationStatus (Switch b){
+        b.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    firebaseHelper.editUser(thisUser, FirebaseObjects.GPS_Follow, true);
+                } else {
+                    firebaseHelper.editUser(thisUser, FirebaseObjects.GPS_Follow, false);
+                }
+            }
+        });
     }
 
-    public void setLocation(boolean location) {
-        this.location = location;
+
+    public void startThread (View view){
+       locationThread thread =new locationThread(10);
+       thread.start();
     }
+    public void stopThread (View view){
+
+    }
+
+
+    class locationThread extends Thread {
+
+        int seconds;
+        locationThread(int seconds){
+            this.seconds
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < seconds; i++){
+                Log.d(TAG, "startThread:" + i);
+                try {
+                    Thread.sleep (1000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class locationRunnable implements Runnable {
+        int seconds;
+
+        locationRunnable (int seconds) {
+            this.seconds = seconds;
+        }
+
+
+
+    }
+
 }
