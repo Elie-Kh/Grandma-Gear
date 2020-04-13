71b9a20ce83917edd6a5dd2ddacf67ee4e20d49f
Kizito <kizitokabanza@gmail.com>
2020-04-13 15:15:55 -0400

    Share Location Request Feature from Follower

--- GrandmaGear/app/src/main/java/com/example/grandmagear/Patient_Main_Lobby/NotificationReceiverDecline.java
+++ GrandmaGear/app/src/main/java/com/example/grandmagear/Patient_Main_Lobby/NotificationReceiverDecline.java
@@ -7,12 +7,10 @@ import android.content.Intent;
 import com.example.grandmagear.FirebaseHelper;
 import com.example.grandmagear.FirebaseObjects;
 
-public class NotificationReceiver extends BroadcastReceiver {
+public class NotificationReceiverDecline extends BroadcastReceiver {
     FirebaseHelper firebaseHelper = new FirebaseHelper();
     @Override
     public void onReceive(Context context, Intent intent) {
-        firebaseHelper.firebaseFirestore.collection(FirebaseHelper.userDB).document(firebaseHelper.getCurrentUserID()).update(FirebaseObjects.GPS_Follow, true);
         firebaseHelper.firebaseFirestore.collection(FirebaseHelper.userDB).document(firebaseHelper.getCurrentUserID()).update(FirebaseObjects.requestLocation, false);
-
     }
 }
