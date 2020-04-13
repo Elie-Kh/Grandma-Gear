d873f4ceabfa613bd4ea22e89e7b906a264f6a1b
abro_ <jjdonofrio@hotmail.com>
2020-04-13 15:22:17 -0400

    finalized styling and added delete confirm fragments

---
+++ GrandmaGear/app/src/main/java/com/example/grandmagear/NotifDeleteFragment.java
@@ -0,0 +1,6 @@
+package com.example.grandmagear;
+
+import androidx.fragment.app.DialogFragment;
+
+public class NotifDeleteFragment extends DialogFragment {
+}
---
+++ GrandmaGear/app/src/main/java/com/example/grandmagear/Patient_Main_Lobby/WearerDeleteFragment.java
@@ -0,0 +1,6 @@
+package com.example.grandmagear.Patient_Main_Lobby;
+
+import androidx.fragment.app.DialogFragment;
+
+public class WearerDeleteFragment extends DialogFragment {
+}
